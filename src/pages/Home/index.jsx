e87fa7410c0f105b6a0767531e35a4bafe48fa5b
ivshxvi <ivhsxvi@gmail.com>
2024-02-22 09:21:24 +0000

    Create home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { useTheme } from "../../contexts";
+
+export default function Home() {
+    const {setTheme} = useTheme()
+
+    
+
+ 
+
+}
