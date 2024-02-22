991e41b179c5f477503e69cea36bc23407baec2d
ivshxvi <ivhsxvi@gmail.com>
2024-02-22 08:52:31 +0000

    Add context

---
+++ src/contexts/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useContext, createContext } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [toggle, setToggle] = useState()
+
+    return (
+        <ThemeContext.Provider value={{ toggle, setToggle}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export const useAuth = () => useContext(ThemeContext)
